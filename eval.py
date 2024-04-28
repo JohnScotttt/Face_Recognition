194df2552ed602b15b87f2e561d1bd01fe9e7e55
JohnScott <johnscott.jiang@gmail.com>
2024-04-28 15:39:11 +0800

    Update model loading and image paths in eval.py and train.py

--- eval.py
+++ eval.py
@@ -11,14 +11,14 @@ args = argparse.Namespace()
 for key in cfg:
     setattr(args, key, cfg[key])
 
-model = FRbackbone.iresnet50().to(args.device)
+model = FRbackbone.get_model(**args.model).to(args.device)
 model.load_state_dict(torch.load(args.load_model))
+model.eval()
 
-img1 = cv2.imread("D:/repos/Face_Recognition/images/img.png")
-img2 = cv2.imread("D:/repos/Face_Recognition/images/img1.jpg")
+img1 = cv2.imread(r"D:\repos\Face_Recognition\images\img1.jpg")
+img2 = cv2.imread(r"D:\repos\Face_Recognition\images\img_0.jpg")
 img1 = transforms.ToTensor()(cv2.resize(img1, (112, 112)))
 img2 = transforms.ToTensor()(cv2.resize(img2, (112, 112)))
-model.eval()
 out1 = model(torch.tensor(img1).unsqueeze(0).to(args.device))
 out2 = model(torch.tensor(img2).unsqueeze(0).to(args.device))
 result = nn.CosineSimilarity()(out1, out2)
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime
 import logging
+import math
 import os
 
 import cv2
@@ -13,32 +14,35 @@ from tqdm import tqdm
 
 import FRbackbone
 import FRloss
-from FRdataset import (FaceRecognitionDataloader, NegativeDataset,
-                       PositiveDataset)
+from FRdata import get_dataloader, get_dataset
 
-cfg = yaml.safe_load(open("cfg.yml", "r"))
+config_path = "config/cwb_10e_11N_nbs63_ir50_f512_ArcCE_s64_m5e-1_gpu.yml"
+
+cfg = yaml.safe_load(open(config_path, "r"))
 args = argparse.Namespace()
 for key in cfg:
     setattr(args, key, cfg[key])
-
-augmentation = []
-for ways in args.augmentation:
-    augmentation.append(eval(ways))
 
 if args.device == "cuda":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 else:
     device = torch.device("cpu")
 
-PD = PositiveDataset(args.root_path, args.description, transforms.Compose(augmentation))
-ND = NegativeDataset(args.root_path, args.description, transforms.Compose(augmentation))
-FRD = FaceRecognitionDataloader(PD, ND, args.neg_batch_size, True)
+augmentation = []
+for ways in args.augmentation:
+    augmentation.append(eval(ways))
+augmentation = transforms.Compose(augmentation)
 
-model = FRbackbone.iresnet50().to(device)
+FRDS = get_dataset(**args.dataloader, transforms=augmentation)
+FRDL = get_dataloader(**args.dataloader, dataset=FRDS, shuffle=True)
+
+model = FRbackbone.get_model(**args.model).to(device)
 if args.load_model is not None:
     model.load_state_dict(torch.load(args.load_model))
-loss_fn = FRloss.FaceRecognitionLoss().to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+loss_fn = FRloss.get_loss_fn(**args.loss_fn).to(device)
+optimizer = torch.optim.AdamW(model.parameters(),
+                              lr=args.lr, weight_decay=args.weight_decay)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, args.scheduler)
 
 if args.log:
     current_time = datetime.datetime.now()
@@ -46,24 +50,17 @@ if args.log:
     log_file_name = os.path.join(args.log_path, f"train{formatted_time}.log")
     logging.basicConfig(filename=log_file_name, filemode="w",
                         format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
-                        datefmt="%d-%m-%Y %H:%M:%S", level=logging.INFO)
+                        datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
 
-logging.info("Start training\n"
-             f"Dataset: {args.dataset}\n"
-             f"Device: {device}\n"
-             f"Load Model: {args.load_model}\n"
-             f"Epochs: {args.epochs}\n"
-             f"Negative Batch Size: {args.neg_batch_size}\n"
-             f"Learning Rate: {args.lr}\n"
-             f"Weight Decay: {args.weight_decay}\n"
-             f"Log Interval: {args.log_interval}\n"
-             f"Save Path: {args.save_path}\n"
-             f"Augmentation: {args.augmentation}")
+info_list = ["Start training\n",]
+with open(config_path, "r") as f:
+    info_list.append(f.read())
+logging.info("".join(info_list))
 
 for epoch in range(args.epochs):
     model.train()
     train_loss = 0
-    bar = tqdm(FRD)
+    bar = tqdm(FRDL)
     for counter, (batch, labels) in enumerate(bar):
         flag = False
         batch = batch.to(device)
@@ -75,12 +72,17 @@ for epoch in range(args.epochs):
         if flag:
             continue
         out_batch = model(batch)
+        out_batch_tem = out_batch
         label = torch.zeros(1, dtype=torch.long).cuda()
-        loss = loss_fn(out_batch, label)
+        loss = loss_fn(out_batch_tem, label)
+        if math.isnan(loss.item()):
+            logging.error(f"Epoch {epoch}/{args.epochs} Batch {counter} Loss is nan")
+            break
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if args.log and counter % args.log_interval == 0:
             logging.info(f"Epoch {epoch}/{args.epochs} Batch {counter} Loss {loss.item()}")
-            bar.set_description(f"Epoch {epoch}/{args.epochs} Batch {counter} Loss {loss.item():.4f}")
-    torch.save(model.state_dict(), os.path.join(args.save_path, f"epoch_{epoch}.pth"))
+        bar.set_description(f"Epoch {epoch}/{args.epochs} Loss {loss.item():.4f}")
+    scheduler.step()
+    torch.save(model.state_dict(), os.path.join(args.save_path, f"{formatted_time}epoch_{epoch}.pth"))
